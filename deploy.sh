2aaaf8712f34afd8dbcbf7665d1f4ccd83989f26
npotta11 <hainaresh@hotmail.com>
2019-10-12 23:49:26 -0500

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t npotta11/multi-client:latest -t npotta11/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t npotta11/multi-server:latest -t npotta11/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t npotta11/multi-worker:latest -t npotta11/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push npotta11/multi-client:latest
-docker push npotta11/multi-server:latest
-docker push npotta11/multi-worker:latest
+docker build -t npotta/multi-client:latest -t npotta/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t npotta/multi-server:latest -t npotta/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t npotta/multi-worker:latest -t npotta/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push npotta/multi-client:latest
+docker push npotta/multi-server:latest
+docker push npotta/multi-worker:latest
 
-docker push npotta11/multi-client:$SHA
-docker push npotta11/multi-server:$SHA
-docker push npotta11/multi-worker:$SHA
+docker push npotta/multi-client:$SHA
+docker push npotta/multi-server:$SHA
+docker push npotta/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=npotta11/multi-server:$SHA
-kubectl set image deployments/client-deployment client=npotta11/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=npotta11/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=npotta/multi-server:$SHA
+kubectl set image deployments/client-deployment client=npotta/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=npotta/multi-worker:$SHA
